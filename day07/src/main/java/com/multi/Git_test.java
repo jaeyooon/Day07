a9ec6c4ecedf5a053052dc4ac297dc557b5ccca4
jaeyooon <111714371+jaeyooon@users.noreply.github.com>
2022-09-21 14:53:46 +0900

    Update Git_test.java

--- day07/src/main/java/com/multi/Git_test.java
+++ day07/src/main/java/com/multi/Git_test.java
@@ -4,6 +4,9 @@ public class Git_test {
 
 	public static void main(String[] args) {
 		System.out.println("TEST");
+		System.out.println("TEST");
+		System.out.println("TEST");
+		System.out.println("TEST");
 	}
 
 }
